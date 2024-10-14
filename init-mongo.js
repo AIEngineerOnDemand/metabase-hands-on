1165da7083c68e4c6656f51a644326cb6970a561
AIEngineerOnDemand <giopantana@gmail.com>
2024-10-14 11:28:53 -0500

    Update init-mongo.js: Set compatibility version and create metabase user

--- init-mongo.js
+++ init-mongo.js
@@ -1,3 +1,24 @@
+// Imposta la versione di compatibilità
+db = db.getSiblingDB('admin');
+db.system.version.updateOne(
+  { _id: "featureCompatibilityVersion" },
+  { $set: { version: "4.4" } }
+);
+
+// Crea l'utente metabase
+db = db.getSiblingDB('metabase');
+db.createUser({
+  user: "metabase",
+  pwd: "metabase",
+  roles: [
+    {
+      role: "readWrite",
+      db: "metabase"
+    }
+  ]
+});
+
+// Crea una collezione di esempio e inserisci alcuni documenti
 db.createCollection('esempio');
 db.esempio.insertMany([
   { nome: 'Esempio 1', valore: 100 },
